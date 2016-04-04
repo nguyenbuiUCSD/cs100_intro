e338544b5222a95d5b0b85dc9a84136f4241426d
Nguyen P Bui <n4bui@ieng6-203.ucsd.edu>
2016-04-04 16:28:00 -0700

    First C++ hello world

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,3 +1,10 @@
-Hello world
-This is my first program with Bitbucket
-Thanks for reading
+/* Hello world
+ * This is my first program with Bitbucket
+ * Thanks for reading
+ */
+#include <iostream>
+
+int main(){
+	std::cout << "Hello world\n";
+	return 0;
+}
